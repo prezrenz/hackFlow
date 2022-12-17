57cb09dfe520f72d99a5af2266a8e8a1f5d31567
prezrenz <perezrenjie20@gmail.com>
2022-12-17 19:29:54 +0800

    Implemented lock key gui

--- tiles/input.gd
+++ tiles/input.gd
@@ -6,9 +6,9 @@ export(int) var intDat
 export(String, "string", "integer") var type
 export(String) var strDat
 
-onready var panel = $PanelContainer
-onready var values = $PanelContainer/Values
-onready var datas = $PanelContainer/Values/Datas
+onready var panel = $Panel
+onready var labels = $Panel/Labels
+#onready var datas = $PanelContainer/Values/Datas
 onready var area = $Area2D
 
 var entered = false
@@ -26,8 +26,8 @@ func _ready():
 	else:
 		data.set_text(str(intDat))
 		
-	datas.add_child(data)
-	datas.add_child(datType)
+	labels.add_child(data)
+	labels.add_child(datType)
 
 #func _process(delta):
 #	if((entered) and (Input.is_mouse_button_pressed(BUTTON_LEFT))):
@@ -47,11 +47,3 @@ func _ready():
 #func _on_Area2D_mouse_exited():
 #	entered = false
 #	print(entered)
-
-func _on_PanelContainer_mouse_entered():
-	panel.show()
-	print("yes")
-
-
-func _on_PanelContainer_mouse_exited():
-	panel.hide()
--- tiles/lock.gd
+++ tiles/lock.gd
@@ -2,15 +2,27 @@ extends Sprite
 
 var tileName = "lock"
 
+onready var keyLabel = $Data
+
 export(String, "string", "integer") var type
 export(int) var keyInt
 export(String) var keyStr
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var keyL = Label.new()
+	
+	if(type == "string"):
+		keyL.set_text(keyStr)
+	elif(type == "integer"):
+		keyL.set_text(str(keyInt))
+	else:
+		pass
+	
+	keyL.set_align(1)
+	keyLabel.add_child(keyL)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
